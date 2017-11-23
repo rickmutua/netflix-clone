0d453f8add5049dfe26a983f88b9a67913a84435
Erick <rick.mutua12@gmail.com>
2017-11-23 12:44:25 +0300

    update added movie class created a requests file minor modifications

--- netflix/settings.py
+++ netflix/settings.py
@@ -23,6 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
+# configure tmdb api_key
+API_KEY = config('API_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
